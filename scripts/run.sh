c2f907c8160763cc5385e22195b06fd26a8c7345
FlorianAinadou <florian.ainadou97@gmail.com>
2021-02-07 14:31:54 +0100

    Correction du run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar ../dsl-antlr/target/CinEditorML.dsl-antlr-1.0-jar-with-dependencies.jar $1 > outputs/$2.ino
+$SHELL
--- scripts/runExternal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-java -jar ../dsl-antlr/target/arduinoml.dsl-antlr-1.0-jar-with-dependencies.jar $1 > outputsExternal/$2.ino
-$SHELL
