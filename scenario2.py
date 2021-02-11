d8cdf2148dab4920668908c802395c0b9f904fa5
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2021-02-11 13:39:03 +0100

    ajout des différents passages avec sous titres

--- scenario2.py
+++ scenario2.py
@@ -1,6 +1,22 @@
 from moviepy.editor import *
 from moviepy.video.io.VideoFileClip import VideoFileClip
+
 myvideoOne = VideoFileClip("atletico_celta.mp4").subclip(23,107)
 myvideoTwo = VideoFileClip("atletico_celta.mp4").subclip(121,141)
-final = CompositeVideoClip([myvideoOne,myvideoTwo])
-final.write_videofile("chain.mp4", fps=myvideoOne.fps)
+
+_txt_begin=TextClip("my title text goes here..", fontsize=70, color='white')
+_txt_begin=_txt_begin.set_position('center').set_duration(10)
+
+_txt_begin_1 = TextClip("my title text goes here..", fontsize=70, color='white')
+_txt_begin_1 =_txt_begin_1.set_position('center').set_duration(10)
+_txt_begin_1 = _txt_begin_1.set_start(40)
+
+_txt_begin_2 = TextClip("my title text goes here..", fontsize=70, color='white')
+_txt_begin_2 =_txt_begin_2.set_position('center').set_duration(15)
+_txt_begin_2 = _txt_begin_2.set_start(-5)
+
+secondclip = CompositeVideoClip([myvideoTwo,_txt_begin_2],size=myvideoOne.size)
+txt_begin = CompositeVideoClip( [myvideoOne,_txt_begin,_txt_begin_1], size=myvideoOne.size)
+
+final = concatenate_videoclips([txt_begin,secondclip],method='compose')
+final.write_videofile("videoscenario2.mp4", fps=30)
