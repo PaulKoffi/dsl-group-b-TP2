37695fe64489e0722029f1084400b4aed7969ccb
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2021-02-13 17:59:43 +0100

    superposition d'un audio sur celui de la vidéo

---
+++ testaudio.py
@@ -0,0 +1,18 @@
+from moviepy.editor import *
+# from moviepy.video.fx import volumex
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+audio_clip = AudioFileClip("songTest.mp4")
+# audio_clip.volumex(0.2)
+my_video_two = VideoFileClip("atletico_celta.mp4").subclip('02:21','02:41')
+audio_clip_2 = my_video_two.audio
+
+
+compo = CompositeAudioClip([audio_clip_2.volumex(1.2),
+                            audio_clip.set_start(5), # start at t=5s]
+                            ])
+
+video2 = my_video_two.set_audio(compo)
+# my_video_two.set_audio(audio_clip)
+video2.write_videofile("testAudio.mp4", fps=30)
+
