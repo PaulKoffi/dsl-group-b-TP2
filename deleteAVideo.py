1b7ba6c1ee9109d17d672ee38ea18030115cd715
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2021-02-17 01:28:06 +0100

    avoir la durée d'un audio

---
+++ deleteAVideo.py
@@ -0,0 +1,3 @@
+import os 
+
+os.remove('testAudio.mp4')
--- testaudio.py
+++ testaudio.py
@@ -2,7 +2,9 @@ from moviepy.editor import *
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
 my_video_two = VideoFileClip("atletico_celta.mp4").subclip('02:21','02:41')
+my_audio = AudioFileClip("songTest.mp4")
+audio_duration = int(my_audio.duration)
 video_duration = int(my_video_two.duration)
 
-print(video_duration)
+print(video_duration, audio_duration)
 
